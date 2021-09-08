d2705b0bf6c62a7ecfc3643d6516ac89f895bea2
ashryan <20115553+ashryan125@users.noreply.github.com>
2021-09-08 16:10:54 -0400

    completed mutations. login, add user, JWT, addThought, addReaction, addFriend

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -26,7 +26,13 @@ const typeDefs = gql `
         username: String
     }
 
+    type Auth {
+        token: ID!
+        user: User
+    }
+
     type Query {
+        me: User
         users: [User]
         user(username: String!): User
         thoughts(username: String): [Thought]
@@ -36,14 +42,12 @@ const typeDefs = gql `
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
+        addThought(thoughtText: String!): Thought
+        addReaction(thoughtId: ID!, reactionBody: String!): Thought
+        addFriend(friendId: ID!): User
     }
-
-    type Auth {
-        token: ID!
-        user: User
-    }
+  
     `;
 
 module.exports = typeDefs;
 
-// 6137be039287158183e089e7   Precious37
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -9,7 +11,8 @@ const app = express();
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 server.applyMiddleware({ app });
